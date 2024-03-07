143e4b34dce12c50d537cc64acf1e5199aebc143
teoaranda <145863288+teoarandaa@users.noreply.github.com>
2024-03-07 22:41:42 +0100

    a

--- home/animacionAparicion.js
+++ home/animacionAparicion.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     var elemento = document.getElementById('textoAnimado');
     var distanciaDesdeElTop = elemento.getBoundingClientRect().top;
-    var distanciaDesdeLaPantalla = window.innerHeight / 1.5; // Ajusta este valor según la posición deseada en la pantalla
+    var distanciaDesdeLaPantalla = window.innerHeight / 1; // Ajusta este valor según la posición deseada en la pantalla
   
     if (distanciaDesdeElTop < distanciaDesdeLaPantalla) {
       elemento.classList.add('mostrar');
